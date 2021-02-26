4816433643923d6ac344fe7b91c473987c46d81c
YeomeoR <76974577+YeomeoR@users.noreply.github.com>
2021-02-26 10:00:24 +0000

    Weather Ninja
    Weather App using JS and AccuWeather API

---
+++ JS weather app - net ninja/scripts/forecast.js
@@ -0,0 +1,25 @@
+// this file is for forecast apis
+
+const key = 'rp7CnrTrRvIyu6U4y8Y85QWgBhI3ZWj6';
+
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
